fc5538af5e3873fa54fd1c9afe361442972e7c9c
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-08 21:55:32 -0800

    Initial Commit

---
+++ Instagram Clone App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram Clone App/Instagram_Clone_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_Clone_AppApp.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_Clone_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
