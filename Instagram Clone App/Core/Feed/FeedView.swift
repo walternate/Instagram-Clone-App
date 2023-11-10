727bd17c9537dbef6346c23ada89b0e12496bd1c
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-09 20:59:03 -0800

    FeedView and SearchView design demo implemented

---
+++ Instagram Clone App/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-08.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
