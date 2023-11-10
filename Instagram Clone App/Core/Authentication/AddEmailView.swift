26753a36e14c21360e18cffc4375856389f0782d
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-09 21:54:53 -0800

    login view demo design implemented

---
+++ Instagram Clone App/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-09.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
---
+++ Instagram Clone App/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-09.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
