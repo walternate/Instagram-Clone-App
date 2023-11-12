b5ac44bb2a82c2d229c6df1cefdad5c0f933b108
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-11 19:55:22 -0800

    cleaned the code for several views, made components out of the code.

---
+++ Instagram Clone App/Core/Components/PostGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PostGridView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-11.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostGridView()
+}
---
+++ Instagram Clone App/Model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-11.
+//
+
+import Foundation
