ce3dc4291e36e7509990947ab0b34f2af7d353e2
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-11 22:38:01 -0800

    connected firebase

---
+++ Instagram Clone App/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-11.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
---
+++ Instagram Clone App/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UploadPostViewModel.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-11.
+//
+
+import Foundation
