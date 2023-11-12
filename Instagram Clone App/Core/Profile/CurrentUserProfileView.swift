69d37cb82c0780b3fc41c534c826175ea5f652dd
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-11 17:29:35 -0800

    search view improved design

---
+++ Instagram Clone App/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUserProfileView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-11.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
