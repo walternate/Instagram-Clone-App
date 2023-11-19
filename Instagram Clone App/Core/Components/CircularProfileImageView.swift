bb22c5d50b41d8b2f6d50ad9209d1b999a16875a
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-19 14:39:53 -0800

    added Kingfisher API and started implementing CircularImageView

---
+++ Instagram Clone App/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-19.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ Instagram Clone App/Services/ImageUploader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageUploader.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-19.
+//
+
+import Foundation
