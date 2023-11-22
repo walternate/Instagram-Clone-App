f7a58f6622fbb6fdc09b67e3b305918f0093b633
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-21 18:16:47 -0800

    implemented feedviewmodel and populated the feed view with real life posts

--- Instagram Clone App/Core/Feed/View/FeedCell.swift
+++ Instagram Clone App/Core/Feed/View/FeedCell.swift
@@ -14,11 +14,7 @@ struct FeedCell: View {
             //image + plus username
             HStack {
                 if let user = post.user {
-                    Image(user.profileImageURL ?? "")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
+                    CircularProfileImageView(user: user, size: .xSmall)
                     Text(user.username)
                         .font(.footnote)
                         .fontWeight(.semibold)
---
+++ Instagram Clone App/Core/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-21.
+//
+
+import Foundation
