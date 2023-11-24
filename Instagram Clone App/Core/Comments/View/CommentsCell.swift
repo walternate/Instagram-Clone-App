8e0b7fa627528bf47bec1f6c530930880a53bb7e
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-23 22:40:34 -0800

    designed the comments view and implemented it with some test comments

---
+++ Instagram Clone App/Core/Comments/View/CommentsCell.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsCell.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-23.
+//
+
+import SwiftUI
+
+struct CommentsCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentsCell()
+}
