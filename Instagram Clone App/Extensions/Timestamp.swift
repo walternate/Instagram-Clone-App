0a1d8180ac0e2cdcc2d69c83b76582554a47ca4f
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-11-26 21:10:47 -0800

    implemented timestamps and constants

---
+++ Instagram Clone App/Extensions/Timestamp.swift
@@ -0,0 +1,8 @@
+//
+//  Timestamp.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-26.
+//
+
+import Foundation
---
+++ Instagram Clone App/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-11-26.
+//
+
+import Foundation
