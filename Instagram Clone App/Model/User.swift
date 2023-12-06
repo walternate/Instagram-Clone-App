acb98352e6252b4bc6a55e06207047e4b9c39113
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-05 22:32:27 -0800

    small changes

--- Instagram Clone App/Model/User.swift
+++ Instagram Clone App/Model/User.swift
@@ -27,7 +27,7 @@ extension User {
     static var MOCK_USERS : [User]  = [
         .init(id: NSUUID().uuidString, email: "blackpanther@gmail.com", username: "blackpanther", profileImageURL: nil, fullname: "Chadwick Boseman", bio: "Wakanda Forever"),
         .init(id: NSUUID().uuidString, email: "batman@gmail.com", username: "batman", profileImageURL: nil, fullname: "Bruce Wayne", bio: "Gotham"),
-        .init(id: NSUUID().uuidString, email: "ironman@gmail.com", username: "ironman", profileImageURL: nil, fullname: "Tony Stark", bio: "Ironman"),
+        .init(id: NSUUID().uuidString, email: "ironman@gmail.com", username: "ironman", profileImageURL: nil, fullname: "Tony Starrrk", bio: "Ironman"),
         .init(id: NSUUID().uuidString, email: "venom@gmail.com", username: "venom", profileImageURL: nil, fullname: nil, bio: "Venom"),
         .init(id: NSUUID().uuidString, email: "aquaman@gmail.com", username: "aquaman", profileImageURL: nil, fullname: "aquaman", bio: "Aquaman")
     ]
