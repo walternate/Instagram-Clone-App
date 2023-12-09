6a85965b08302ddb08c4c527e6625b55fa759e15
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-08 23:32:12 -0800

    small changes

---
+++ Instagram Clone App/Core/Components/View/UserListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserListView.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-12-07.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListView()
+}
