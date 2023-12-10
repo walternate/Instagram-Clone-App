e5f9066dfdc68665ba927f7591aad91da3d2fd08
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-09 21:45:30 -0800

    started implementing notifications UI

---
+++ Instagram Clone App/Core/Notifications/Views/Notifications.swift
@@ -0,0 +1,18 @@
+//
+//  Notifications.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-12-09.
+//
+
+import SwiftUI
+
+struct Notifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notifications()
+}
---
+++ Instagram Clone App/Core/Notifications/Views/NotificationsCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsCell.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-12-09.
+//
+
+import SwiftUI
+
+struct NotificationsCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsCell()
+}
