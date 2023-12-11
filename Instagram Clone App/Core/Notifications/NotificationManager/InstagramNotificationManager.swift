fea07353f683578fcffe101c9013e69dc4d1175a
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-10 22:20:32 -0800

    implemented the notifications view and completed the first MVP

---
+++ Instagram Clone App/Core/Notifications/NotificationManager/InstagramNotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  InstagramNotificationManager.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-12-10.
+//
+
+import Foundation
---
+++ Instagram Clone App/Core/Notifications/Service/InstagramNotificationService.swift
@@ -0,0 +1,8 @@
+//
+//  InstagramNotificationService.swift
+//  Instagram Clone App
+//
+//  Created by Ikbal Demirdoven on 2023-12-10.
+//
+
+import Foundation
